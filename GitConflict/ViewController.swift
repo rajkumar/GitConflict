3c28d95789b29c3dc1efcad16dc5e5efd844f841
sunarc <sunarc@mac-mini.local>
2017-06-22 16:31:21 +0530

    Added TableView Class

--- GitConflict/ViewController.swift
+++ GitConflict/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Print Print Commit")
+        print("Hello My Second Commit")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
